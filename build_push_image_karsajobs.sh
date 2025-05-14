1bd94ed1f8e673b6b88c8c9e57d4fcf16ad1693c
rhakagesa <rhakagesa@gmail.com>
2025-05-14 23:08:14 +0700

    add karsajobs script to build an push image

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#mendefinisikan variabel image, tag dan username 
+IMAGE_NAME=karsajobs
+IMAGE_TAG=latest
+DOCKER_USERNAME=rhakagesa
+
+#melakukan build image
+echo -e "Building image $IMAGE_NAME:$IMAGE_TAG"
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+
+#merubah nama dan membuat image baru dengan sesuai dengan format docker hub
+echo -e "\nChange image and make a new image $IMAGE_NAME:$IMAGE_TAG to $DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
+docker tag $IMAGE_NAME:$IMAGE_TAG $DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG
+
+#menampilkan daftar image
+echo -e "\nList image"
+docker images
+
+#melakukan login ke docker hub
+echo -e "\nLogin to Docker Hub"
+echo -e $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+
+#melakukan push image
+echo -e "\nPush image to Docker Hub" 
+docker push $DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG
+echo -e "\nDone"
